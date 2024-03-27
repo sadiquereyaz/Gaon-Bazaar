ece2a21a4e369ef9ef5615c745d494b59693e27a
sadiquereyaz <mdsadique47@gmail.com>
2024-03-27 23:42:05 +0530

    bottom bar

--- app/src/main/java/com/reyaz/gaonbazar/navigation/BottomNavGraph.kt
+++ app/src/main/java/com/reyaz/gaonbazar/navigation/BottomNavGraph.kt
@@ -1,11 +1,11 @@
 package com.reyaz.gaonbazar.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.reyaz.gaonbazar.model.Constant.CATEGORY
-import com.reyaz.gaonbazar.model.Constant.MAIN
 import com.reyaz.gaonbazar.model.Constant.ORDER
 import com.reyaz.gaonbazar.model.Constant.SELLER
 import com.reyaz.gaonbazar.screens.CategoryScreen
@@ -14,18 +14,27 @@ import com.reyaz.gaonbazar.screens.OrderScreen
 import com.reyaz.gaonbazar.screens.SellerScreen
 
 @Composable
-fun SetupNavigation(
+fun BottomNavGraph(
+    navController: NavHostController
 ) {
-    val navController = rememberNavController()
 
     NavHost(
         navController = navController,
-        startDestination = MAIN,
-        route = "main_route"
+        startDestination = CATEGORY,
+        route = "category_route"
 
     ) {
-        composable(route = MAIN) {
-            MainScreen()
+
+        composable(route = CATEGORY) {
+            CategoryScreen(navController = navController)
+
+        }
+        composable(route = ORDER) {
+            OrderScreen(navController = navController)
+
+        }
+        composable(route = SELLER) {
+           SellerScreen(navController = navController)
         }
     }
 }
